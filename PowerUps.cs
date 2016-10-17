d5db2aad10d6e6cb73f8619fccef497e713a959e
Jacob Lofgren <jakeshmearlof4@gmail.com>
2016-10-17 11:53:01 -0600

    did lots of stuff

---
+++ PowerUps.cs
@@ -0,0 +1,10 @@
+using System;
+public class PowerUps {
+    int power = 10;
+    int duration = 5;
+    string name = "PowerUps";
+
+    public void RunPowerUp () {
+        Console.WriteLine("using PowerUp");
+    }
+}
