750eda9697541164980b228c7c993f6da6fc908d
Jacob Lofgren <jakeshmearlof4@gmail.com>
2016-10-19 11:53:13 -0600

    followed instrucitions, no idea what is just did

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
-﻿using System;
-
-namespace ConsoleApplication
-{
+﻿   using System;
     public class Program
     {
+
+        public static PowerUp Health = new PowerUp();
+        public static PowerUp Ammo = new PowerUp();
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          Health.duration = 10;
+          Health.RunPowerUp();
+          Console.WriteLine(Health.duration);
+          Ammo.RunPowerUp();
+
+          Health.RechargePowerUp(20);
+          Ammo.RechargePowerUp(100);
         }
     }
-}
